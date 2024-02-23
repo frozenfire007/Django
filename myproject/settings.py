250279fc300263ef585b19c2a4e654b60a135c9a
Ranjan Lamichhane <ranjanlamichhane@Ranjans-MacBook-Pro.local>
2024-02-23 16:24:41 +0545

    url

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4f6$3v@admu@ivh!m0r5p%#&t_v-y19doum8a(w_-8ofiercc*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp'
 ]
 
 MIDDLEWARE = [
